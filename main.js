62997a6f1d8346ca6fb8ae4dc32d09ef0d42466c
mahi1212 <mahi121.mr@gmail.com>
2021-08-13 15:50:58 +0600

    accordion js done

--- main.js
+++ main.js
@@ -16,7 +16,12 @@ $(document).ready(function(){
         }
     });
 
-    
+    $('.accordion-header').click(function(){
+        $('.accordion .accordion-body').slideUp();
+        $(this).next('.accordion-body').slideDown();
+        $('.accordion .accordion-header span').text('+');
+        $(this).children('span').text('-');
+    });
 
 
 });
